24cf9ddd9ecd92930cfe7ceb96f3a4455455bedc
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-06-28 17:33:27 +0900

    Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,14 @@
 # $1 The dir path to remove files
 # $2 Newline-delimited list of paths
 
-echo "Hello $1 $2"
+# Make "new line" the delimiter so "for loop" will loop over each line instead over each word.
+# Default value for IFS is space and tab so it would loop over each word instead over each line.
+IFS='
+'
+
+for file in $(ls $1)
+do
+  echo $file
+done
+
+# Determine the file is in $2
